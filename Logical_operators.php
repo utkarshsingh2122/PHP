4bf6b8be1b1c14ba290973ae967b47cfde90c22e
utkarshsingh2122 <107643326+utkarshsingh2122@users.noreply.github.com>
2023-01-28 23:51:31 +0530

    Add files via upload

---
+++ Logical_operators.php
@@ -0,0 +1,10 @@
+<?php
+  $age=25;
+  if(!($age>=28)){
+    echo"You are eligible";
+  }
+
+  if($age>=28 xor $age==15){
+    echo"You are eligible";
+  }
+ ?>
---
+++ max_and_min.php
@@ -0,0 +1,15 @@
+
+<?php
+/*2 Write a PHP script to create a static web page
+to find the maximum and minimum marks from the following
+ set of arrays.
+
+Sample arrays :
+$marks1 = array(360,310,310,330,313,375,456,111,256);*/
+
+$array = array(360,310,310,330,313,375,456,111,256);
+echo("Maximum number :- ".max($array));
+echo("<br>");
+echo("Minimum number :- ".min($array));
+
+ ?>
