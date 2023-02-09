8778292bb8d09a6b1b4088fd1cf23e852e8169ba
utkarshsingh2122 <107643326+utkarshsingh2122@users.noreply.github.com>
2023-02-09 23:54:15 +0530

    Add files via upload

---
+++ Functions_with_parameters.php
@@ -0,0 +1,15 @@
+<?php
+      /*  function add($a ,$b){
+          echo $a+$b , "<br>";
+        }
+        add(10,15);
+        add(15,20);*/
+
+        // Second function
+
+        function hello($a){
+          echo "Hello $a <br>";
+        }
+        hello("World");
+        hello("Utkarsh");
+ ?>
